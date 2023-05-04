d1c2dbb962a50af1ec7ff5c5fecff6469a4dd5d8
Ian Gierhan <89673758+igierhan-mscs@users.noreply.github.com>
2023-05-04 18:58:24 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript for the navigation bar
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
